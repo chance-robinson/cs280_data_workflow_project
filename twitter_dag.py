688c1f695c51f71f740ddbbf966316afb9cf9ca1
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-01-30 16:35:36 -0700

    setup

---
+++ twitter_dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+import logging as log
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+
+# def first_task_function():
+#     log.info("Second Dag")
+#     name = "Enter dag \# here"
+#     log.info(f"Dag \#{dag}")
+#     return
+
+with DAG(
+    dag_id="project_lab_1_etl",
+    schedule_interval="0 9 * * *",
+    start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
+    catchup=False,
+) as dag:
+    # start_task = DummyOperator(task_id="start_task")
+    # first_task = PythonOperator(task_id="first_task", python_callable=first_task_function)
+    # middleware1_task = PythonOperator(task_id="middlware1_task", python_callable=middleware1_task_function)
+    # middleware2_task = PythonOperator(task_id="middleware2_task", python_callable=middleware2_task_function)
+    # last_task = PythonOperator(task_id="last_task", python_callable=last_task_function)
+    # end_task = DummyOperator(task_id="end_task")
+
+# start_task >> first_task >> [middleware1_task, middleware2_task] >> last_task >> end_task
+start_task >> end_task
