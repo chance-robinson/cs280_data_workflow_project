4f7e2f309b47eb2a3def34fc9f162c6f3eb4930e
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-01-30 16:36:20 -0700

    setup

--- twitter_dag.py
+++ twitter_dag.py
@@ -16,12 +16,12 @@ with DAG(
     start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
     catchup=False,
 ) as dag:
-    # start_task = DummyOperator(task_id="start_task")
+    start_task = DummyOperator(task_id="start_task")
     # first_task = PythonOperator(task_id="first_task", python_callable=first_task_function)
     # middleware1_task = PythonOperator(task_id="middlware1_task", python_callable=middleware1_task_function)
     # middleware2_task = PythonOperator(task_id="middleware2_task", python_callable=middleware2_task_function)
     # last_task = PythonOperator(task_id="last_task", python_callable=last_task_function)
-    # end_task = DummyOperator(task_id="end_task")
+    end_task = DummyOperator(task_id="end_task")
 
 # start_task >> first_task >> [middleware1_task, middleware2_task] >> last_task >> end_task
 start_task >> end_task
