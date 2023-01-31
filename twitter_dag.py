416ae19c49fa273e114652cb4d05984f947665ef
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-01-31 03:09:12 -0700

    changed interval

--- twitter_dag.py
+++ twitter_dag.py
@@ -87,7 +87,7 @@ def upload_databox():
 
 with DAG(
     dag_id="project_lab_1_etl",
-    schedule_interval="*/1 * * * *", # schedule_interval="0 9 * * *",
+    schedule_interval="0 9 * * *", # schedule_interval="*/1 * * * *",
     start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
     catchup=False,
 ) as dag:
