a8c547b498396a6e38cb5df6a6ecc74b3cdf207f
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-01-31 00:19:22 -0700

    added sorting

--- twitter_dag.py
+++ twitter_dag.py
@@ -7,11 +7,31 @@ from airflow.models import TaskInstance
 import requests
 import pandas as pd
 from google.cloud import storage
+import json
+
+def flatten_json(data_dict, matching_data, keys_to_match):
+    for key, value in data_dict.items():
+        if type(value) is dict:
+            flatten_json(value, matching_data, keys_to_match)
+        else:
+            if key in keys_to_match:
+                matching_data.append(f"{key}: {value}")
+    return matching_data
+
+def iterate_json_list(data_dict, keys_to_match):
+    match_list = []
+    for dicti in data_dict:
+        matches = []
+        if type(dicti) is dict:
+            flatten_json(dicti, matches, keys_to_match)
+        matches = {x.split(": ")[0]: x.split(": ")[1] for x in matches}
+        matches = dict(sorted(matches.items()))
+        match_list.append(matches)
+    return pd.DataFrame(match_list)
 
 def get_twitter_api(ti: TaskInstance, **kwargs):
     user_ids = Variable.get("TWITTER_USER_IDS", deserialize_json=True)
     tweet_ids = Variable.get("TWITTER_TWEET_IDS", deserialize_json=True)
-    # my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
     my_bearer_token = "AAAAAAAAAAAAAAAAAAAAAHrdlQEAAAAAu2vIvvakLLbGqgsBXAcjwyK6XQo%3Db3PuxzKm28q0lZQUZ6N55qocL7t2YQ4no6FEET9nfURgIb2YkC"
     header_token = {"Authorization": f"Bearer {my_bearer_token}"}
     user_requests = [requests.get(f"https://api.twitter.com/2/users/{id}?user.fields=public_metrics,profile_image_url,username,id,description", headers=header_token).json() for id in user_ids]
@@ -22,12 +42,16 @@ def get_twitter_api(ti: TaskInstance, **kwargs):
     logging.info(tweet_requests)
 
 def transform_twitter_api_data_func(ti: TaskInstance, **kwargs):
-    user_requests = pd.DataFrame(data=ti.xcom_pull(key="user_requests", task_ids="get_twitter_api_data_task"))
-    tweet_requests = pd.DataFrame(data=ti.xcom_pull(key="tweet_requests", task_ids="get_twitter_api_data_task"))
     client = storage.Client()
     bucket = client.get_bucket("c-r-apache-airflow-cs280")
-    bucket.blob("data/user_requests.csv").upload_from_string(user_requests.to_csv(index=False), "text/csv")
-    bucket.blob("data/tweet_requests.csv").upload_from_string(tweet_requests.to_csv(index=False), "text/csv")
+    users = pd.DataFrame(data=ti.xcom_pull(key="user_requests", task_ids="get_twitter_api_data_task"))
+    tweets = pd.DataFrame(data=ti.xcom_pull(key="tweet_requests", task_ids="get_twitter_api_data_task"))
+    tweet_header_list = ['retweet_count', 'reply_count', 'like_count', 'quote_count', 'impression_count', 'tweet_id', 'text', 'id']
+    user_header_list = ['followers_count','following_count','tweet_count','listed_count','name','username','id']
+    user_matching_data = iterate_json_list(json.loads(users), user_header_list)
+    tweet_matching_data = iterate_json_list(json.loads(tweets), tweet_header_list)
+    bucket.blob("data/user_requests.csv").upload_from_string(user_matching_data.to_csv(index=False), "text/csv")
+    bucket.blob("data/tweet_requests.csv").upload_from_string(tweet_matching_data.to_csv(index=False), "text/csv")
 
 
 with DAG(
--- twitter_test.py
+++ twitter_test.py
@@ -1,17 +1,6 @@
 import requests
 import json
-
-my_bearer_token = "AAAAAAAAAAAAAAAAAAAAAHrdlQEAAAAAu2vIvvakLLbGqgsBXAcjwyK6XQo%3Db3PuxzKm28q0lZQUZ6N55qocL7t2YQ4no6FEET9nfURgIb2YkC"
-header_token = {"Authorization": f"Bearer {my_bearer_token}"}
-user_id = "44196397"
-api_url = f"https://api.twitter.com/2/users/{user_id}"
-request = requests.get(api_url, headers=header_token)
-# print(request.json())
-
-
-# def get_auth_header():
-#     my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
-#     return {"Authorization": f"Bearer {my_bearer_token}"}
+import pandas as pd
 
 def get_twitter_api():
     user_ids = [44196397,62513246,11348282,12044602,2557521]
@@ -22,7 +11,38 @@ def get_twitter_api():
     tweet_requests = []
     for id in tweet_ids:
         tweet_requests.append(requests.get(f"https://api.twitter.com/2/tweets/{id}?tweet.fields=author_id,text,public_metrics", headers=header_token).json())
-    print(user_requests)
+    user_requests = json.dumps(user_requests)
+    tweet_requests = json.dumps(tweet_requests)
+    return user_requests, tweet_requests
 
-    
-get_twitter_api()
+users,tweets = get_twitter_api()
+
+# Define a list of keys to match
+tweet_header_list = ['retweet_count', 'retweet_count', 'like_count', 'quote_count', 'impression_count']
+user_header_list = ['followers_count','following_count','tweet_count','listed_count']
+
+# Create an empty list to store the matching key-value pairs
+tweet_match = []
+user_match = []
+
+def flatten_json2(data_dict, matching_data, keys_to_match):
+    print(pd.json_normalize(data_dict).head())
+    for key, value in data_dict.items():
+        if type(value) is dict:
+            flatten_json2(value, matching_data, keys_to_match)
+        else:
+            if key in keys_to_match:
+                matching_data.append((key, value))
+    return matching_data
+
+# Define a function that will recursively loop through the nested dictionaries in the JSON object
+def flatten_json(data_dict, matching_data, keys_to_match):
+    for dicti in data_dict:
+        if type(dicti) is dict:
+            flatten_json2(dicti, matching_data, keys_to_match)
+
+# Flatten the nested dictionaries in the JSON object and get matching key-value pairs
+user_matching_data = flatten_json(json.loads(users), user_match, user_header_list)
+# tweet_matching_data = flatten_json(json.loads(tweets), tweet_match, tweet_header_list)
+# Display the matching key-value pairs
+# print(user_match)
