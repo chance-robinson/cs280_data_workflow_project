cf7b96fa5bffbd4b9f945517377f865070ecfb67
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-01-31 02:42:25 -0700

    testing

--- twitter_dag.py
+++ twitter_dag.py
@@ -73,7 +73,8 @@ def upload_databox():
             logging.info(val)
             for idx, item in enumerate(val):
                 logging.info(f'{idx}: {item}')
-                dbox.push(f'UserMetric: {val[name_index]}:{header[idx]}', item)
+                if type(item) == int or type(item) == float:
+                    dbox.push(f'UserMetric {val[name_index]} {header[idx]}', item)
     # with fs.open('gs://c-r-apache-airflow-cs280/data/tweet_requests.csv', 'r') as f:
     #     reader = csv.reader(f)
     #     header = next(reader)
