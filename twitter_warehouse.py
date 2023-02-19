5a350b965e7cc57007308c148356d5b1e2e8671a
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-02-19 08:06:28 -0700

    test

--- twitter_warehouse.py
+++ twitter_warehouse.py
@@ -25,6 +25,7 @@ def call_api(ti: TaskInstance, **kwargs):
     # tweets = json.loads(ti.xcom_pull(key="tweets", task_ids="load_data_task"))
     user_requests = [requests.get(f"https://api.twitter.com/2/users/{id[0]}?user.fields=public_metrics,created_at", headers=header_token).json() for id in users]
     user_latest_tweet = [requests.get(f"https://api.twitter.com/2/users/{id[0]}/tweets?max_results=5", headers=header_token).json() for id in users]
+    print(user_latest_tweet[0]["data"])
     user_latest_tweet = [[id[0], user_latest_tweet[idx]["data"][0]["id"]] for idx,id in enumerate(users)]
     user_latest_updated = [requests.get(f"https://api.twitter.com/2/tweets/{id[1]}?tweet.fields=public_metrics,created_at,author_id", headers=header_token).json() for id in user_latest_tweet]
     ti.xcom_push("user_info", json.dumps(user_requests))
