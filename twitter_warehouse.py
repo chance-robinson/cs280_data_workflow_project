00f3bc4bab18c3f11951b9c20fd6fc3dff3c50dc
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-02-19 07:19:53 -0700

    test

--- twitter_warehouse.py
+++ twitter_warehouse.py
@@ -22,10 +22,15 @@ def call_api(ti: TaskInstance, **kwargs):
     my_bearer_token = "AAAAAAAAAAAAAAAAAAAAAHrdlQEAAAAAu2vIvvakLLbGqgsBXAcjwyK6XQo%3Db3PuxzKm28q0lZQUZ6N55qocL7t2YQ4no6FEET9nfURgIb2YkC"
     header_token = {"Authorization": f"Bearer {my_bearer_token}"}
     users = json.loads(ti.xcom_pull(key="users", task_ids="load_data_task"))
-    # tweets = data=ti.xcom_pull(key="tweets", task_ids="load_data_task")
-    # user_requests = [requests.get(f"https://api.twitter.com/2/users/{id}?user.fields=public_metrics", headers=header_token).json() for id in users]
-    for i in users:
-        print(i)
+    tweets = json.loads(ti.xcom_pull(key="tweets", task_ids="load_data_task"))
+    user_requests = [requests.get(f"https://api.twitter.com/2/users/{id[0]}?user.fields=public_metrics,created_at", headers=header_token).json() for id in users]
+    user_latest_tweet = [requests.get(f"https://api.twitter.com/2/users/{id[0]}/tweets?max_results=5", headers=header_token).json() for id in users]
+    user_latest_tweet = [[id[0], users[idx]['data'][0]['id']] for idx,id in enumerate(users)]
+    user_latest_updated = [requests.get(f"https://api.twitter.com/2/tweets/{id[1]}?tweet.fields=public_metrics,created_at", headers=header_token).json() for id in user_latest_tweet]
+    # https://api.twitter.com/2/users/:id/tweets?max_results=5
+    print(user_requests)
+    print(" ")
+    print(user_latest_updated)
 
 def transform_data():
     return 0
