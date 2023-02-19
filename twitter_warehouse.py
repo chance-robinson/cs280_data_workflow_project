aa162953251908129d6d47047b2d427b6de2df22
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-02-19 09:09:02 -0700

    test

--- twitter_warehouse.py
+++ twitter_warehouse.py
@@ -25,12 +25,10 @@ def call_api(ti: TaskInstance, **kwargs):
     # tweets = json.loads(ti.xcom_pull(key="tweets", task_ids="load_data_task"))
     user_requests = [requests.get(f"https://api.twitter.com/2/users/{id[0]}?user.fields=public_metrics,created_at", headers=header_token).json() for id in users]
     user_latest_tweet = [requests.get(f"https://api.twitter.com/2/users/{id[0]}/tweets?max_results=5", headers=header_token).json() for id in users]
-    print(users)
-    print(user_latest_tweet)
-    user_latest_tweet = [[id[0], user_latest_tweet[idx]] for idx,id in enumerate(users)]
-    user_latest_updated = [requests.get(f"https://api.twitter.com/2/tweets/{id[1]}?tweet.fields=public_metrics,created_at,author_id", headers=header_token).json() for id in user_latest_tweet]
-    ti.xcom_push("user_info", json.dumps(user_requests))
-    ti.xcom_push("latest_tweets_info", json.dumps(user_latest_updated))
+    user_latest_tweet = [user_latest_tweet[idx]["data"][0]['id'] for idx,id in enumerate(users)]
+    user_latest_updated = [requests.get(f"https://api.twitter.com/2/tweets/{id}?tweet.fields=public_metrics,created_at,author_id", headers=header_token).json() for id in user_latest_tweet]
+    # ti.xcom_push("user_info", json.dumps(user_requests))
+    # ti.xcom_push("latest_tweets_info", json.dumps(user_latest_updated))
 
 def transform_data(ti: TaskInstance, **kwargs):
     user_info = json.loads(ti.xcom_pull(key="user_info", task_ids="call_api_task"))
