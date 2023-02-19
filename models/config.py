d45cb6a6ab75dc809666fc72005c101a26a98f39
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-02-19 02:48:23 -0700

    test

--- models/config.py
+++ models/config.py
@@ -8,7 +8,8 @@ def create_dburl():
   password = Variable.get("DATABASE_HOSTNAME")
   port = Variable.get("DATABASE_PORT")
   database = Variable.get("DATABASE_NAME")
-  return f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{database}"
+#   return f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{database}"
+  return f"postgresql+psycopg2://brighamyoung2@gmail.com:robinson@0.0.0.0:5432/twitter"
 
 engine = create_engine(create_dburl())
 Session = sessionmaker(bind=engine)
