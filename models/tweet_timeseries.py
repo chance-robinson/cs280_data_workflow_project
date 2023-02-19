5120b61bdf78f815e6b3da3511d4b48d5af26e61
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-02-18 22:38:21 -0700

    testing task 1+2

---
+++ models/tweet_timeseries.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Date, Float
+
+Base = declarative_base()
+
+class Tweet_Timeseries(Base):
+    __tablename__ = "tweet_timeseries"
+    id = Column(Integer, primary_key=True, not_null=True)
+    tweet_id = Column(Integer, not_null=True)
+    retweet_count = Column(Integer, not_null=True)
+    favorite_count = Column(Integer, not_null=True)
+    date = Column(Date, not_null=True)
+
+    def __repr__(self) -> str:
+        return f"Tweet_Timeseries(id={self.id}, tweet_id={self.tweet_id}, retweet_count={self.retweet_count}, favorite_count={self.favorite_count}, date={self.date})"
---
+++ twitter_warehouse.py
@@ -0,0 +1,64 @@
+from airflow import DAG
+import logging
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+from airflow.models import TaskInstance
+import requests
+from models.config import Session #You would import this from your config file
+from models.users import User
+from models.tweet import Tweet
+import json
+
+def load_data(ti: TaskInstance, **kwargs):   
+    session = Session()
+    users = session.query(User).all() 
+    logging(users)
+    tweets = session.query(Tweet).all() 
+    session.close()
+    ti.xcom_push("users", json.dumps(users))
+    ti.xcom_push("tweets", json.dumps(tweets))
+
+def call_api(ti: TaskInstance, **kwargs):
+    my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN")
+    header_token = {"Authorization": f"Bearer {my_bearer_token}"}
+    users = data=ti.xcom_pull(key="users", task_ids="load_data_task")
+    tweets = data=ti.xcom_pull(key="tweets", task_ids="load_data_task")
+    logging(users)
+    user_requests = [requests.get(f"https://api.twitter.com/2/users/{id}?user.fields=public_metrics,profile_image_url,username,id,description", headers=header_token).json() for id in users.user_id]
+    tweet_requests = [requests.get(f"https://api.twitter.com/2/tweets/{id}?tweet.fields=author_id,text,public_metrics", headers=header_token).json() for id in tweets.tweet_id]
+
+def transform_data():
+    return 0
+    
+def write_data():
+    return 0
+
+with DAG(
+    dag_id="project_lab_2_data_warehouse",
+    schedule_interval="0 9 * * *", # schedule_interval="*/2 * * * *", "0 9 * * *",
+    start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"), # start_date=pendulum.datetime(2023, 1, 26, tz="US/Pacific"),
+    catchup=False,
+) as dag:
+    load_data_task = PythonOperator(
+        task_id="load_data_task", 
+        python_callable=load_data,
+        provide_context=True
+    )
+    call_api_task = PythonOperator(
+        task_id="call_api_task", 
+        python_callable=call_api,
+        provide_context=True
+    )
+    transform_data_task = PythonOperator(
+        task_id="transform_data_task",
+        python_callable=transform_data,
+        provide_context=True
+    )
+    write_data_task = PythonOperator(
+        task_id="write_data_task",
+        python_callable=write_data,
+        provide_context=True
+    )
+
+load_data_task >> call_api_task >> transform_data_task >> write_data_task
