d8b41595e95f99bce594389b4e83f491adf5cac1
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-02-21 01:28:42 -0700

    step4

--- twitter_warehouse.py
+++ twitter_warehouse.py
@@ -107,6 +107,7 @@ def write_data():
         print(myDict)
         if (q.filter(User_Timeseries.user_id==val[myDict['data.id']])) and (q.all()):
             record = q.filter(User_Timeseries.user_id==val[myDict['data.id']]).first()
+            print(record)
             record.followers_count = val[myDict['data.public_metrics.followers_count']]
             record.following_count = val[myDict['data.public_metrics.following_count']]
             record.tweet_count = val[myDict['data.public_metrics.tweet_count']]
