6bcee62cf2e3f9cea8d3779408a316360aee7711
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-02-21 01:22:41 -0700

    step4

--- twitter_warehouse.py
+++ twitter_warehouse.py
@@ -103,8 +103,11 @@ def write_data():
         session = Session()
         myDict = header_index_vals(header, match_headers)
         q = session.query(User_Timeseries)
+        print(q)
+        print(q.all())
         if (q.filter(User_Timeseries.user_id==val[myDict['data.id']])) and (q.all()):
-            record = q.filter(User_Timeseries.user_id==val[myDict['data.id']]).one()
+            record = q.filter(User_Timeseries.user_id==val[myDict['data.id']]).first()
+            print(val)
             record.followers_count = val[myDict['data.public_metrics.followers_count']]
             record.following_count = val[myDict['data.public_metrics.following_count']]
             record.tweet_count = val[myDict['data.public_metrics.tweet_count']]
