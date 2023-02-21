fcbdf5c26f11e9be063fa413f388ac43a86fd3a1
chance <chance@Chance-Robinson-CS280.myguest.virtualbox.org>
2023-02-21 02:20:24 -0700

    step4

--- models/config.py
+++ models/config.py
@@ -3,13 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 def create_dburl():
-  hostname = Variable.get("DATABASE_USERNAME")
-  username = Variable.get("DATABASE_PASSWORD")
-  password = Variable.get("DATABASE_HOSTNAME")
+  username = Variable.get("DATABASE_USERNAME")
+  password = Variable.get("DATABASE_PASSWORD")
+  hostname = Variable.get("DATABASE_HOSTNAME")
   port = Variable.get("DATABASE_PORT")
   database = Variable.get("DATABASE_NAME")
   return f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{database}"
-  # return f"postgresql+psycopg2://postgres:robinson@0.0.0.0:5432/twitter"
 
 engine = create_engine(create_dburl())
 Session = sessionmaker(bind=engine)
